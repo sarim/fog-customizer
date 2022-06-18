fe9a1895ab1260bb860d37de3a295cc13b8daf84
Sarim Khan <sarim2005@gmail.com>
2022-06-18 08:41:52 +0600

    keybind added

--- src/main/java/me/sarim/FogCustomizer.java
+++ src/main/java/me/sarim/FogCustomizer.java
@@ -3,18 +3,48 @@ package me.sarim;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import me.sarim.config.ClothConfigScreenFactory;
+import me.sarim.config.Config;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.text.Text;
+import net.minecraft.world.dimension.DimensionTypes;
 
 public class FogCustomizer implements ClientModInitializer {
     public static final String MOD_ID = "fog-customizer";
     public static final String MOD_NAME = "Fog Customizer";
-    public static final String MOD_CONF = "fog-customizer.json";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static Config CONFIG = null;
+
+	private static final KeyBindingHandler toggleFogOverrideKeyBinding = new KeyBindingHandler(
+		"key." + FogCustomizer.MOD_ID + ".toggle_fog_override",
+		"key.categories.misc",
+		(e) -> {
+            if (e.player.clientWorld.getRegistryKey().getValue().equals(DimensionTypes.OVERWORLD_ID)) {
+                CONFIG.Generic.override = !CONFIG.Generic.override;
+                e.player.sendMessage(Text.literal("Fog Customizer Override: " + CONFIG.Generic.override), true);
+            } else {
+                CONFIG.Thick.override = !CONFIG.Thick.override;
+                e.player.sendMessage(Text.literal("Fog Customizer Override: " + CONFIG.Thick.override), true);
+            }
+        }
+	);
+
+    private static final KeyBindingHandler openFogConfigKeyBinding = new KeyBindingHandler(
+		"key." + FogCustomizer.MOD_ID + ".open_fog_config",
+		"key.categories.misc",
+		(e) -> {
+            e.setScreenAndRender(
+                ClothConfigScreenFactory.genConfig(e.currentScreen)
+            );
+        }
+	);
 
     @Override
     public void onInitializeClient()
     {
-        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+        CONFIG = Config.load();
+		toggleFogOverrideKeyBinding.register();
+        openFogConfigKeyBinding.register();
     }
-
 }
---
+++ src/main/java/me/sarim/KeyBindingHandler.java
@@ -0,0 +1,49 @@
+package me.sarim;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public final class KeyBindingHandler implements ClientTickEvents.EndTick {
+	private final KeyBinding keyBinding;
+	private boolean isHoldingKey = false;
+	private final PressHandler pressHandler;
+	
+	public KeyBindingHandler(String translationKey, String category, PressHandler pressHandler) {
+		this(translationKey, InputUtil.UNKNOWN_KEY.getCode(), category, pressHandler);
+	}
+	
+	public KeyBindingHandler(String translationKey, int defaultKeyCode, String category, PressHandler pressHandler) {
+		this.keyBinding = new KeyBinding(
+			translationKey,
+			InputUtil.Type.KEYSYM,
+			defaultKeyCode,
+			category
+		);
+		this.pressHandler = pressHandler;
+	}
+	
+	public void register() {
+		KeyBindingHelper.registerKeyBinding(keyBinding);
+		ClientTickEvents.END_CLIENT_TICK.register(this);
+	}
+	
+	@Override
+	public final void onEndTick(MinecraftClient e) {
+		if (keyBinding.isPressed()) {
+			if (!isHoldingKey) {
+				pressHandler.handlePress(e);
+			}
+			isHoldingKey = true;
+		} else {
+			isHoldingKey = false;
+		}
+	}
+	
+	@FunctionalInterface
+	public interface PressHandler {
+		void handlePress(MinecraftClient e);
+	}
+}
